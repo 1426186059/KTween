159dd4d38c83c8f6d285f661d6f31d06e040d3b9
xuke2026 <825126369@qq.com>
2026-06-21 19:13:36 +0800

    o

--- UE5.KTween/UE5_KTween/Plugins/KTween/Source/KTween/Public/KTween.h
+++ UE5.KTween/UE5_KTween/Plugins/KTween/Source/KTween/Public/KTween.h
@@ -51,7 +51,7 @@ public:
         {
             if (IsValid() && mOtherTween.IsValid())
             {
-                mInnerPtr.Pin()->AppendTween(mOtherTween);
+                mInnerPtr.Pin()->AppendTween(mOtherTween.Get());
             }
             else
             {
